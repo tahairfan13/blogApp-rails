0d865bc5e3d55692975da2f99dbccd2b4f9b448c
Taha Ali <tahairfan1993@gmail.com>
2018-12-14 19:10:31 +0500

    changed maximum length

--- app/models/post.rb
+++ app/models/post.rb
@@ -6,8 +6,7 @@ class Post < ApplicationRecord
   has_many :likes , dependent: :destroy
   #validations
   validates :title, presence: true ,length: {maximum: 35}
-  validates :description ,presence: true ,length: {minimum: 10, maximum: 1000} #remember to change the minimum later
-  mount_uploader :image, AvatarUploader
+  validates :description ,presence: true ,length: {minimum: 100, maximum: 10000} #remember to change the minimum later
 
   scope :filter_category, -> (category_id) {where(category_id: category_id.to_i) if category_id.present?} #Scope not used just for future references
 end
